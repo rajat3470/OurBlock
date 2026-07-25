d94f0cee2a9016ff51d79d383bf439950d21ebbf
‘Rajat9610’ <vrajat9610@gmail.com>
2026-07-25 12:04:44 +0530

    android build configuration

--- apps/mobile/android/app/src/main/java/com/mohallamitr/app/MainActivity.kt
+++ apps/mobile/android/app/src/main/java/com/mohallamitr/app/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.ourblock.app
+package com.mohallamitr.app
 import expo.modules.splashscreen.SplashScreenManager
 
 import android.os.Build
--- apps/mobile/android/app/src/main/java/com/mohallamitr/app/MainApplication.kt
+++ apps/mobile/android/app/src/main/java/com/mohallamitr/app/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.ourblock.app
+package com.mohallamitr.app
 
 import android.app.Application
 import android.content.res.Configuration
